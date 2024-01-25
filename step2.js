15e27d9574de84fe6eec6fde38311eaccb9ead80
ryzac <rzc044@gmail.com>
2024-01-25 08:03:39 -0600

    Step 2 Complete

---
+++ step2.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const axios = require('axios')
+
+function cat(path) {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.log(`Error reading ${path}: ${err}`);
+            process.kill(1)
+        }
+        console.log("DATA:", data)
+    })
+}
+
+async function webCat(url) {
+    try {
+        let res = await axios.get(url)
+        console.log(res.data)
+    } catch (err) {
+        console.log(`Error fetching ${url}: ${err}`);
+        process.kill(1)
+    }
+}
+
+let path = process.argv[2]
+
+if (path.slice(0, 4) === 'http') {
+    webCat(path);
+} else {
+    cat(path)
+}
